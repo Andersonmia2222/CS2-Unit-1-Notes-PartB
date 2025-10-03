0213e22d8de3ca8c5fca9824db470863c7bf0f85
Andersonmia2222 <mia.anderson2026@gbwl.org>
2025-10-03 12:43:01 +0000

    1.8 notes

--- Main.java
+++ Main.java
@@ -1,5 +1,14 @@
+/** 
+ * @author Mia A  
+ * @since Oct 3 2025
+ * @version 1.0
+ * Notes for topics 1.8-1.15
+*/
 public class Main {
-
+/**
+ * Main method is the entry point for program 
+ * @Paran input is an array of strings 
+ */
    public static void main(String []args) {
       System.out.println("Hello World");
    }
