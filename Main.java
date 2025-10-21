2eaa0fa8b985771fb9695ac6aace00745753c462
Andersonmia2222 <mia.anderson2026@gbwl.org>
2025-10-21 14:27:23 +0000

    update

--- Main.java
+++ Main.java
@@ -10,6 +10,41 @@ public class Main {
  * @Paran input is an array of strings 
  */
    public static void main(String []args) {
-      System.out.println("Hello World");
+      String lastWord = message.substring(8);
+      System.out.println(lastWord);
+
+      String message1 = "This is cool";
+      String message2= "This is not cool"; 
+
+      int indexOfNot1 = message1.indexOf("not");
+      int indexOfNot2 = message2.indexOf("not"); 
+
+      System.out.println("index of not in message1: " + indexOfNot1); //-1
+      System.out.println("index of not in message2: " + indexOfNot2); //8
+
+      //.equals() allows you to compare the contents of two string objects 
+      // it checks EVERY CHARACTER in both, then returns a boolean answer
+
+      boolean areTheyEqual = message1.equals(message2);
+      System.out.println("does m1 equal m2? + areTheyEqual"); //false
+// order doest matter here 
+
+areTheyEqual = message2.equals(message1);
+System.out.println(areTheyEqual);
+
+//you can compare string variable to a string literal
+areTheyEqaul = message1.equals("this is cool");
+system.out.println(areTheyEqual); //true 
+
+// .compareTo compares two strings, charcter by charcter 
+//eturns an int representing alphabetical order
+
+System.out.println(message1.compareTo("Z"));//-6
+System.out.println(message1.compareTo("t"));//-32
+System.out.println(message1.compareTo("A"));//19 
+// neg numbers mean the other string comes after 
+//the original/calling string, message1
+
+
    }
-}
+} 
